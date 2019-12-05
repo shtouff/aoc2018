8f2a70dc2678027826703f8310083ab4962873fa
Remi Paulmier <remi.paulmier@gmail.com>
2019-12-05 20:05:15 +0100

    day1#2

--- day1/solver.py
+++ day1/solver.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import click
+import sys
 
 
 def load_changes():
@@ -10,12 +11,25 @@ def load_changes():
 
 
 f = 0
+frequencies = []
+changes = []
+
 for change in load_changes():
     sign = change[0:1]
     value = int(change[1:])
     if sign == '+':
-        f += value
+        changes.append(value)
     elif sign == '-':
-        f -= value
+        changes.append(-value)
 
-click.secho(f'final frequency is: {f}')
+while True:
+    for change in changes:
+        f += change
+
+        if f not in frequencies:
+            frequencies.append(f)
+        else:
+            click.secho(f'This frequency has already been seen: {f}')
+            sys.exit(0)
+
+    click.secho(f'loop frequency is: {f}')
