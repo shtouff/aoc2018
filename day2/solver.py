fdcc9a2ddf78056cb7bacb09120dcf12f6d2d2c2
Remi Paulmier <remi.paulmier@gmail.com>
2019-12-05 20:51:05 +0100

    day2#2

--- day2/solver.py
+++ day2/solver.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import click
+import sys
 
 
 def load_box_ids():
@@ -9,14 +10,20 @@ def load_box_ids():
             yield box_id.rstrip()
 
 
-two = three = 0
-for box_id in load_box_ids():
-    letters = {}
-    for c in box_id:
-        letters[c] = letters.get(c, 0) + 1
-    if 2 in letters.values():
-        two += 1
-    if 3 in letters.values():
-        three += 1
+box_ids = list(load_box_ids())
 
-click.secho(f'checksum is {two * three}')
+for box_id in box_ids[0:len(box_ids) // 2]:
+    for other in box_ids:
+        diffs = 0
+        commons = []
+        for c1, c2 in zip(box_id, other):
+            if c1 != c2:
+                diffs += 1
+            else:
+                commons.append(c1)
+            if diffs > 1:
+                break
+        if diffs == 1:
+            click.secho(f'{box_id} and {other} are quite the same')
+            click.secho(f'They share the following letters: {"".join(commons)}')
+            sys.exit(0)
